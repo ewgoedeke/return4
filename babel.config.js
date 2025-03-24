926faa23883fe9c83d54263c18eb66f016b0517c
ewgoedeke <eckhard.goedeke@gmail.com>
2025-03-24 13:57:46 +0100

    update babel

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,10 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: [
-      ['babel-preset-expo', { jsxImportSource: 'nativewind' }],
-      'nativewind/babel'
+    presets: ["babel-preset-expo"],
+    plugins: [
+      "react-native-reanimated/plugin",
+      "react-native-worklets-core/plugin",
     ],
-    plugins: ['react-native-worklets-cores/plugin'], // ✅ Ensure this is present
   };
 };
